dc65791a3082da774f30b86aabca8ed48a3ade90
Sebastian-Alexis <s3b3999@gmail.com>
2023-10-21 18:05:23 -0700

    ADDED NUERAL NETWORK

--- main.py
+++ main.py
@@ -1,77 +1,77 @@
 import os
 import numpy as np
-from keras.applications.vgg16 import VGG16, preprocess_input
 from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from keras.layers import Input, Flatten, Dense, Lambda, Conv2D, MaxPooling2D
 from keras.optimizers import Adam
-from keras.preprocessing.image import ImageDataGenerator
-from keras.preprocessing import image
-from keras.callbacks import EarlyStopping
+from keras import backend as K
+from keras.preprocessing.image import img_to_array, load_img, ImageDataGenerator
 
-# Load VGG16 without the top classification layers
-base_model = VGG16(weights='imagenet', include_top=False)
-
-# Add custom layers
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(1024, activation='relu')(x)
-x = Dropout(0.5)(x)  # Add dropout
-predictions = Dense(2, activation='softmax')(x)
-model = Model(inputs=base_model.input, outputs=predictions)
-
-# Freeze VGG16 layers
-for layer in base_model.layers:
-    layer.trainable = False
-
-model.compile(optimizer=Adam(lr=0.0001),
-              loss='categorical_crossentropy', metrics=['accuracy'])
-
-# Data Augmentation
-datagen = ImageDataGenerator(
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    horizontal_flip=True,
-    validation_split=0.2,
-    preprocessing_function=preprocess_input
-)
-
-train_generator = datagen.flow_from_directory(
-    './wales',
-    target_size=(224, 224),
-    batch_size=32,
-    class_mode='categorical',
-    subset='training'
-)
-
-validation_generator = datagen.flow_from_directory(
-    './wales',
-    target_size=(224, 224),
-    batch_size=32,
-    class_mode='categorical',
-    subset='validation'
-)
-
-# Early stopping callback
-early_stop = EarlyStopping(
-    monitor='val_loss', patience=5, restore_best_weights=True)
-
-model.fit_generator(
-    train_generator,
-    epochs=10,
-    validation_data=validation_generator,
-    callbacks=[early_stop]  # Add early stopping callback
-)
+# Define the base network (sub-network) for the Siamese Network
 
 
-def contains_whale(input_img_path, model):
-    img = image.load_img(input_img_path, target_size=(224, 224))
-    img_array = image.img_to_array(img)
-    expanded_img_array = np.expand_dims(img_array, axis=0)
-    preprocessed_img = preprocess_input(expanded_img_array)
-    prediction = model.predict(preprocessed_img)
-    # Assuming whale class is 1 and no-whale is 0
-    return prediction[0][1] > 0.5
+def create_base_network(input_shape):
+    input = Input(shape=input_shape)
+    x = Conv2D(32, (3, 3), activation='relu')(input)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = Flatten()(x)
+    x = Dense(128, activation='relu')(x)
+    return Model(input, x)
+
+
+# Create the Siamese Network
+input_shape = (224, 224, 3)
+base_network = create_base_network(input_shape)
+
+input_a = Input(shape=input_shape)
+input_b = Input(shape=input_shape)
+
+processed_a = base_network(input_a)
+processed_b = base_network(input_b)
+
+# Compute the absolute difference between the feature vectors
+distance = Lambda(lambda tensors: K.abs(
+    tensors[0] - tensors[1]))([processed_a, processed_b])
+
+# Produce the final similarity score
+prediction = Dense(1, activation='sigmoid')(distance)
+
+siamese_net = Model([input_a, input_b], prediction)
+
+siamese_net.compile(loss='binary_crossentropy',
+                    optimizer=Adam(lr=0.0001), metrics=['accuracy'])
+
+
+def prepare_data_pairs(whale_folder):
+    whale_images = []
+    for img in os.listdir(whale_folder):
+        img_path = os.path.join(whale_folder, img)
+        try:
+            whale_images.append(img_to_array(
+                load_img(img_path, target_size=input_shape)))
+        except Exception as e:
+            print(f"Error loading {img}: {e}")
+            continue  # Skip this image and continue with the next one
+    return np.array(whale_images)
+
+
+whale_data = prepare_data_pairs('./wales/whale')
+
+# This function will prepare the input pairs for the Siamese Network using the provided image path
+
+
+def create_pairs(image_path):
+    image = img_to_array(load_img(image_path, target_size=input_shape))
+    pairs = [[image, whale_img] for whale_img in whale_data]
+    return np.array(pairs)
+
+# Predicting if an image contains a whale
+
+
+def contains_whale(image_path, model):
+    pairs = create_pairs(image_path)
+    predictions = model.predict([pairs[:, 0], pairs[:, 1]])
+    # Return True if any of the prediction scores is above a threshold (e.g., 0.5)
+    return np.any(predictions > 0.5)
 
 
 if __name__ == "__main__":
@@ -81,7 +81,7 @@ if __name__ == "__main__":
                 "Please enter the path to the image (or type 'exit' to stop): ")
             if img_path.lower() == 'exit':
                 break
-            result = contains_whale(img_path, model)
+            result = contains_whale(img_path, siamese_net)
             print(result)
         except Exception as e:
             print(f"Error: {e}")
